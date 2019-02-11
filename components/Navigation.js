72f0afb088af05605ce2c696909f1eae5a3882a0
robert2030ca <robert2030ca@gmail.com>
2019-02-10 20:47:14 -0500

    material ui integration start

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,21 +1,56 @@
 import Link from 'next/link'
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import NoSsr from '@material-ui/core/NoSsr';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
 
 const linkStyle = {
   marginRight: 15
 }
 
+function LinkTab(props) {
+  return <Tab component="a" onClick={event => event.preventDefault()} {...props} />;
+}
+
 const Navigation = () => (
-    <div>
-        <Link href="/">
-          <a style={linkStyle}>Home</a>
-        </Link>
-        <Link href="/about">
-          <a style={linkStyle}>About</a>
-        </Link>
-        <Link href="/chatbot">
-          <a style = {linkStyle}> Chatbot </a>
-        </Link>
-    </div>
+    //<div>
+        // <Link href="/">
+        //   <a style={linkStyle}>Home</a>
+        // </Link>
+        // <Link href="/about">
+        //   <a style={linkStyle}>About</a>
+        // </Link>
+        // <Link href="/chatbot">
+        //   <a style = {linkStyle}> Chatbot </a>
+        // </Link>
+    //</div>
+
+    <NoSsr>
+        <div>
+          <AppBar position="static">
+            <Tabs variant="fullWidth">
+              //<Tab>
+                <Link href="/">
+                  <a style={linkStyle}>Home</a>
+                </Link>
+              //</Tab>
+              //<Tab>
+              <Link href="/about">
+                <a style={linkStyle}>About</a>
+              </Link>
+              //</Tab>
+              //<Tab>
+                <Link href="/chatbot">
+                  <a style = {linkStyle}> Chatbot </a>
+                </Link>
+              //</Tab>
+            </Tabs>
+          </AppBar>
+        </div>
+      </NoSsr>
 )
 
 export default Navigation
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Layout from '../components/Layout.js'
+import Button from '@material-ui/core/Button';
 
 export default () => (
     <Layout>
-       <p>Hello Next.js</p>
+      <p>Hello Next.js</p>
+        <Button variant = 'contained' color = 'blue'>
+          Click Me!
+        </Button>
     </Layout>
 )
