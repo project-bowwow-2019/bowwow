9d96135a909d6192d663c0e5dcc6ec6222373ec7
robert2030ca <robert2030ca@gmail.com>
2019-02-21 17:48:00 -0500

    started the custom questions insert

--- server/src/controllers/chatbotCreateController.js
+++ server/src/controllers/chatbotCreateController.js
@@ -4,12 +4,18 @@ const log = require('../log');
 const handler = require('./handlerController');
 const uuidv4 = require('uuid/v4');
 var pg = require('pg');
+const path = require('path');
+const dialogflow = require('dialogflow');
+
 pg.defaults.ssl = true;
 
-var databaseURL = env.databaseURI;
+
+const databaseURL = env.databaseURI;
+const appRoot = path.join(__dirname,'../../../')
 
 //get all business categories
 exports.getBusinessCategory = function(req,res){
+
   log.info('getBusinessCategory is called')
   var client = new pg.Client({
     connectionString: databaseURL
@@ -137,9 +143,37 @@ exports.postUserChats = async function(req, res){
       connectionString: databaseURL
     });
     client.connect();
+
+    //find the agent the business is supposed to have
+
+    var agentName;
+    const querySelectAgent = 'SELECT agent FROM business_type WHERE category = $1 AND subtype = $2';
+    const selectAgentValues = [businessType.category, businessType.subtype];
+    client.query(querySelectAgent, selectAgentValues)
+    .then(result=>{
+      if(result.rows.length>1){
+        log.info('more than 1 agent is selected, something is wrong, check database!')
+        res.status(500).json('more than 1 agent selected some how');
+      }else if(result.rows.length<1){
+        log.info('no agent is selected, something is wrong, check database!')
+        res.status(500).json('no agent selected some how');
+      } else {
+        agentName = result.rows[0].agent;
+        log.info(agentName + ' is selected');
+      }
+    })
+    .catch(err => {
+      log.info('error at query agent name, error: ' + err);
+      if (err.statusCode>=100 && err.statusCode<600) {
+        res.status(err.statusCode).json(err);
+      } else {
+        res.status(500).json(err);
+      }
+    })
+
     //insert the busines information into business_info table
-    const queryInsertInfo= 'INSERT INTO business_info(business_id, business_category, business_subtype, subscription_type) VALUES ($1, $2, $3, $4)';
-    const infoValues = [userID, businessType.category, businessType.subtype, 'trial']
+    const queryInsertInfo= 'INSERT INTO business_info(business_id, business_category, business_subtype, subscription_type, business_agent) VALUES ($1, $2, $3, $4, $5)';
+    const infoValues = [userID, businessType.category, businessType.subtype, 'trial', agentName]
     client.query(queryInsertInfo, infoValues)
     .then(result=>{
       log.info('business info entered');
@@ -179,8 +213,46 @@ exports.postUserChats = async function(req, res){
         }
       })
     }
+
+    //put in the custom questions into the business intent table
+    //first need to check if any of the custom questions already match an intent
+    //if the intent is matched through dialogflow, the intent and the response will be added to intent tabel
+    //if the intent is not deteced, the new question need to be added to the agent and a "random" intent
+    //name needs to be assigned to the agent, this intent name and the resopnse then go into the intent table
+    if(req.body.customQuestionsDone){
+      var dialogflowProjectId;
+      var dialogflowCredentialPath;
+
+      const queryAgentInfo = 'SELECT TOP 1 * FROM dialogflow_agent WHERE agent = $1';
+      const queryAgentInfoValues = [agentName];
+
+      client.query(queryAgentInfo, queryAgentInfoValues)
+      .then(result=>{
+        log.info(result.rows[0].project_id + ' is selected');
+        dialogflowProjectId=result.rows[0].project_id;
+        dialogflowCredentialPath = path.join(appRoot,result.rows[0].credential_path);
+        console.log(dialogflowCredentialPath)
+      })
+      .catch(err=>{
+        log.info('error at query agent name, ' + err);
+        if (err.statusCode>=100 && err.statusCode<600) {
+          res.status(err.statusCode).json(err);
+        } else {
+          res.status(500).json(err);
+        }
+      })
+
+      var valuesMatrix=[];
+      for(i=0;i<customQuestions.length;i++){
+        //send the question to dialogflow
+
+      }
+
+    }
   }
 }
+
+
 
 // expand(3, 2) returns "($1, $2), ($3, $4), ($5, $6)"
 function expand(rowCount, columnCount, startAt=1){
@@ -193,4 +265,34 @@ function flatten(arr){
   var newArr = []
   arr.forEach(v => v.forEach(p => newArr.push(p)))
   return newArr
+}
+
+async function detectIntent(projectId, filePath, userId, question){
+  const sessionClient = new dialogflow.SessionsClient({
+    projectId: projectId,
+    keyFileName: filePath
+  });
+  const sessionPath = sessionClient.sessionPath(projectId, userId);
+
+  const request = {
+    session: sessionPath,
+    queryInput:{
+      text: {
+        text: question,
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected Intent');
+  const result = responses[0].queryResult;
+  console.log(`Query: ${result.queryText}`);
+  console.log(`Response: ${result.fulfillmentText}`);
+  if(result.intent){
+    console.log(` Intent: ${result.intent.displayName}`);
+  } else {
+    console.log('No intent matched');
+  }
+  return(result.intent.displayName);
 }
